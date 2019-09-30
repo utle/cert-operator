3584cf8d3500332d112344dc407ec45481467311
Ut Le <utle@us.ibm.com>
2019-09-30 17:03:36 -0500

    Update update-generated.sh

--- tmp/codegen/update-generated.sh
+++ tmp/codegen/update-generated.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 vendor/k8s.io/code-generator/generate-groups.sh \
 deepcopy \
-github.com/redhat-cop/cert-operator/pkg/generated \
-github.com/redhat-cop/cert-operator/pkg/apis \
+github.com/utle/cert-operator/pkg/generated \
+github.com/utle/cert-operator/pkg/apis \
 cache:v1alpha1 \
 --go-header-file "./tmp/codegen/boilerplate.go.txt"
